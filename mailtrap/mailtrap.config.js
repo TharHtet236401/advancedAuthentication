24d18716e05eb66579c181ac3ea990b24506add6
TharHtet236401 <tharhteta3@gmail.com>
2024-11-13 03:02:04 +0700

    setConfig and emailTemplate

--- mailtrap/mailtrap.config.js
+++ mailtrap/mailtrap.config.js
@@ -5,26 +5,11 @@ dotenv.config();
 
 const TOKEN = process.env.MAILTRAP_TOKEN;
 
-const client = new MailtrapClient({
+export const client = new MailtrapClient({
   token: TOKEN,
 });
 
-const sender = {
+export const sender = {
   email: "hello@demomailtrap.com",
   name: "Mailtrap Test",
 };
-const recipients = [
-  {
-    email: "tharhtet6401@gmail.com",
-  }
-];
-
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    text: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
